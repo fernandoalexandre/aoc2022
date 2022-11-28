ef56e2ed69b8930a6c0c4523cc4cb213d383e6e5
Fernando Alexandre <f.alexandre@almightybuserror.com>
2022-11-28 20:50:21 +0000

    Added helper comment

--- cmd/fetch.go
+++ cmd/fetch.go
@@ -19,12 +19,9 @@ var day int
 var fetchCmd = &cobra.Command{
 	Use:   "fetch",
 	Short: "Fetches the current day's input",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long: `Fetches the current day's input, note that an environment variable "AOC_TOKEN" is required in a .env file with a format of "session=AAAXXXXXCCCCVVVV..."
+  
+This value can be obtained by inspecting the requests in adventofcode.com while logged in with the "cookie" header.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		log.Println("fetch called")
 
--- cmd/root.go
+++ cmd/root.go
@@ -12,13 +12,10 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "aoc2022",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Application used to solve Advent of Code 2022",
+	Long: `Command line used to learn go while solving Advent of Code 2022
+	
+Each day will be included as a new command and both parts of the problem will execute.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
