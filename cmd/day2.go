64f2ca33918140bebba8e64ced9c73335a3afca4
Fernando Alexandre <f.alexandre@almightybuserror.com>
2022-12-02 08:28:18 +0000

    started day2

---
+++ cmd/day2.go
@@ -0,0 +1,67 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/spf13/cobra"
+)
+
+// day2Cmd represents the day2 command
+var day2Cmd = &cobra.Command{
+	Use:   "day2",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("day2 called")
+	},
+}
+
+func readfile_d2(file_name string) []int {
+	readFile, err := os.Open(file_name)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	var fileLines []int
+
+	for fileScanner.Scan() {
+		val, err := strconv.Atoi(fileScanner.Text())
+		if err != nil {
+			val = -1
+		}
+		fileLines = append(fileLines, val)
+	}
+
+	readFile.Close()
+
+	return fileLines
+}
+
+func init() {
+	rootCmd.AddCommand(day2Cmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// day2Cmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// day2Cmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
