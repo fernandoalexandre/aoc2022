68ddc541f89b84355c8d2847064f019a75d2509d
Fernando Alexandre <fernandoalexandre@users.noreply.github.com>
2022-12-12 18:16:43 +0000

    In progress day11

--- cmd/day12.go
+++ cmd/day12.go
@@ -1,11 +1,13 @@
 /*
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -22,7 +24,113 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("day12 called")
+
+		test_map := d12_readfile("inputs/day12/input_example")
+		actual_map := d12_readfile("inputs/day12/input")
+
+		d12p1(test_map)
 	},
+}
+
+type MapNode struct {
+	value rune
+	x, y  int
+	links []*MapNode
+}
+
+func d12_get_str(x, y int) string {
+	return fmt.Sprintf("%d,%d", x, y)
+}
+
+func (a MapNode) d12_can_move(target MapNode) bool {
+	return Abs(int(a.value)-int(target.value)) <= 1
+}
+
+func d12_parse_map(input_map []string) (output_map map[string]MapNode, start_node MapNode, dest_node MapNode) {
+	output_map = make(map[string]MapNode)
+	for y, line := range input_map {
+		for x, slot := range line {
+			val, found := output_map[d12_get_str(x, y)]
+			if !found {
+				val = MapNode{slot, x, y, []*MapNode{}}
+
+				if val.value == 'S' {
+					val.value = 'a'
+					start_node = val
+				} else if val.value == 'E' {
+
+					dest_node = val
+				}
+
+				output_map[d12_get_str(x, y)] = val
+			}
+
+			if val.value == 'S' {
+				val.value = 'a'
+				start_node = val
+			} else if val.value == 'E' {
+				val.value = 'z'
+				dest_node = val
+			}
+
+			// x y+1
+			if y < len(input_map) {
+				r_val, found := output_map[d12_get_str(x, y+1)]
+				if !found {
+					val = MapNode{slot, x, y + 1, []*MapNode{}}
+					output_map[d12_get_str(x, y+1)] = val
+				}
+
+				if val.d12_can_move(r_val) {
+					val.links = append(val.links, &r_val)
+					r_val.links = append(r_val.links, &val)
+				}
+			}
+
+			// x+1 y
+			if x < len(line) {
+				t_val, found := output_map[d12_get_str(x+1, y)]
+				if !found {
+					val = MapNode{slot, x + 1, y, []*MapNode{}}
+					output_map[d12_get_str(x+1, y)] = val
+				}
+
+				if val.d12_can_move(t_val) {
+					val.links = append(val.links, &t_val)
+					t_val.links = append(t_val.links, &val)
+				}
+			}
+		}
+	}
+	return output_map, start_node, dest_node
+}
+
+func d12_readfile(file_name string) (content []string) {
+	readFile, err := os.Open(file_name)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer readFile.Close()
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		content = append(content, fileScanner.Text())
+	}
+	return content
+}
+
+func find_shortest_route() {
+
+}
+
+func d12p1(raw_map []string) {
+	log.Println("Part 1")
+	parsed_map, start, dest := d12_parse_map(raw_map)
+
 }
 
 func init() {
